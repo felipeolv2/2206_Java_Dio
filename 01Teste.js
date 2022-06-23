6de73323c8ab18a3b44c3f3608370733104919ab
felipeolv2 <felipeoliveira.v2@gmail.com>
2022-06-23 18:14:53 -0300

    início do projeto web/java

---
+++ 01Teste.js
@@ -0,0 +1,19 @@
+// Duas barras é um comentário de linha
+
+/*
+Barra asterisco é um comentário de mais de uma linha, tag de comentário.
+*/
+
+var x = 0;
+var y = 1;
+var z = 2;
+
+function soma (a,b){
+    return a+b;
+}
+
+
+soma(4,3); //7
+
+console.log("Hello World!");
+console.log(y+z);
